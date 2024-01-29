a8500b40370ac14a19374339bc7badb110f5f37b
AKG <1925184+jives@users.noreply.github.com>
2024-01-29 07:42:54 +0100

    Adds first SDL integration test

--- src/main.c
+++ src/main.c
@@ -32,8 +32,32 @@ int main(int argc, char **argv)
     FILE *log_file = fopen("nyanGB.instr.log", "w");
 
     signal(SIGINT, sig_handler);
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Event event;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
+    }
+
+    if (SDL_CreateWindowAndRenderer(320, 240, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
+    }
+
     while (keep_running)
     {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_QUIT)
+        {
+            keep_running = false;
+        }
+
         gb_log_state(&gb, log_file, false);
         cpu_run(&gb);
 
@@ -42,9 +66,18 @@ int main(int argc, char **argv)
             printf("%c", mem_read_byte(&gb, 0xFF01));
             mem_write_byte(&gb, 0xFF02, 0x00);
         }
+
+        // SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        // SDL_RenderClear(renderer);
+        // SDL_RenderPresent(renderer);
     }
 
     fclose(log_file);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
